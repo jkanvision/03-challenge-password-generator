1b55bf5b397c7630b5d1f79f6cd320d920bca0a1
JK code <112551853+jkanvision@users.noreply.github.com>
2022-10-03 18:10:38 -0400

    changes vars from strings to arrays and adds random value generator

--- script.js
+++ script.js
@@ -1,73 +1,55 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-
 // Assignment Code
-
 var generateBtn = document.querySelector("#generate");
-var numerical = "0123456789";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var symbol = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var userSelection = "";
+const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
+const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var userSelection = [];
 
-// prompts user to select the length of the generated password 
+// prompts user to select the length of the generated password and alerts of an invalid selection
 function generatePassword() {
+  var gendPass = "";
   var numOfCharacters = Number(window.prompt ("How many characters would you like your password to be? \(Please choose a number between 8 and 128.\)"));
   if (numOfCharacters < 8 || numOfCharacters > 128 || !numOfCharacters) {
     window.alert ("Your selection not valid. Please try again.");
     return;
   }
-  else {
-    // var length = userSelection.length;
-    generatePassword;
-  }
   
-
-
-
-  // confirm whether or not to include lowercase
+  //  confirms inclusion of lowercase letters and adds them to array named userSelection
   var includeLow = window.confirm ("Do you want to include lowercase letters?");
     if (includeLow == true) {
-      userSelection.concat(includeLow);
+      userSelection = userSelection.concat(lowercase);
     }
   
 
-  // confirm whether or not to include uppercase
+  // confirms inclusion of uppercase letters and adds them to array named userSelection
   var includeUpper = window.confirm ("Do you want to include uppercase letters?");
     if (includeUpper == true) {
-      userSelection.concat(includeUpper);
+      userSelection = userSelection.concat(uppercase);
     }
 
-  // confirm whether or not to include numbers
+  // confirms inclusion of numbers and adds them to array named userSelection
   var includeNum = window.confirm ("Do you want to include numbers?");
     if (includeNum == true) {
-      userSelection.concat(includeNum);
+      userSelection = userSelection.concat(numberChars);
     }
 
-  // confirm whether or not to include symbols
-  var includeSym = window.confirm ("Do you want to include symbols?");
-  if (includeSym == true) {
-    userSelection.concat(includeSym);
+  // confirms inclusion of symbols and adds them to array named userSelection
+  var includeSym = window.confirm ("Do you want to include any special characters?");
+    if (includeSym == true) {
+    userSelection = userSelection.concat(specialChars);
+    }
+
+  // iterates through array containing all possible values available for password generator 
+  console.log(userSelection);
+
+  // generates random password with length that = user selection
+  for (var i = 0; i < numOfCharacters; i++) {
+    
+    gendPass += userSelection[Math.floor(Math.random() * (userSelection.length - 1))];
+    }
+    return gendPass;
   }
-
-  
-  // userSelection(Math.floor(Math.random() * userSelection.length));
-}
-
 
 
 
